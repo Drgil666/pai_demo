9b4ff1b2a45aa12c27626b229f1a461ad9988791
DrGil666 <2574105038@qq.com>
2026-07-07 10:37:33 +0800

    Feats:新增评论类、通知类。

---
+++ src/main/java/com/example/pai_demo/entity/Comment.java
@@ -0,0 +1,51 @@
+package com.example.pai_demo.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author GilbertYoung
+ * @date 2026/07/07 09:54
+ */
+
+/**
+ * 评论类
+ * 顶级评论
+ * |----回复
+ * |----回复的回复
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Entity
+@Table(name = "comment")
+public class Comment extends BaseEntity {
+    /**
+     * 评论对应的文章id
+     */
+    @Column(name = "article_id", nullable = false)
+    private Integer articleId;
+    /**
+     * 评论对应的用户id
+     */
+    @Column(name = "user_id", nullable = false)
+    private Integer userId;
+    /**
+     * 评论内容
+     */
+    @Column(name = "content", nullable = false)
+    private String content;
+    /**
+     * 评论对应的顶级id
+     */
+    @Column(name = "top_comment_id", nullable = false)
+    private Integer topCommentId;
+    /**
+     * 评论对应的上级id
+     */
+    @Column(name = "parent_comment_id", nullable = false)
+    private Integer parentCommentId;
+}
---
+++ src/main/java/com/example/pai_demo/entity/Notify.java
@@ -0,0 +1,38 @@
+package com.example.pai_demo.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author GilbertYoung
+ * @date 2026/07/07 10:28
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Entity
+@Table(name = "notify")
+public class Notify extends BaseEntity {
+    /**
+     * 触发操作的用户id
+     */
+    @Column(name = "operate_user_id", nullable = false)
+    private Integer operateUserId;
+    /**
+     * 被通知的用户id
+     */
+    @Column(name = "notify_user_id", nullable = false)
+    private Integer notifyUserId;
+    /**
+     * 通知的消息内容
+     */
+    @Column(name = "content", nullable = false)
+    private String content;
+    /**
+     * 通知的类型(0-系统,1-评论,2-回复,3-点赞,4-收藏,5-关注)
+     */
+    private Integer type;
+}
